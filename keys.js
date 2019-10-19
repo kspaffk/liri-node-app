664ea7b3ce98a57de5044e9d0b2901655b746033
tworoose <51271998+kspaffk@users.noreply.github.com>
2019-10-19 14:52:23 -0600

    movie this now working

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-console.log('this is loaded');
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.obdb_key = process.env.OMDB_KEY
--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
 var moment = require("moment");
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 
@@ -25,6 +26,7 @@ switch (requestType) {
             
             for (var i = 0; i < data.tracks.items.length; i++) {
               if (data.tracks.items[i].name.toLowerCase() === searchItem.toLowerCase()) {
+                console.log("  ------------  ");
                 console.log("Song Name: " + data.tracks.items[i].name);
                 console.log(
                     "Artist: " + data.tracks.items[i].artists[0].name
@@ -38,7 +40,7 @@ switch (requestType) {
                 } else {
                     console.log("No song preview is available.");
                 }
-                console.log(" --------------------------- ");
+                console.log("  ------------  ");
                 return false;
               }
             }
@@ -50,9 +52,11 @@ switch (requestType) {
         var queryURL = "https://rest.bandsintown.com/artists/" + searchItem + "/events?app_id=codingbootcamp";
         axios.get(queryURL).then(function(response) {
           if (response.data[0] != undefined) {
+            console.log("  ------------  ")
             console.log("Venue Name: " + response.data[0].venue.name);
             console.log("Venue Location: " + response.data[0].venue.city + ", " + response.data[0].venue.country);
             console.log("Venue Name: " + moment(response.data[0].datetime).format("MM/DD/YYYY"));
+            console.log("  ------------  ");
             return false;
           }
           console.log("Your band is not touring right now.")
@@ -61,9 +65,38 @@ switch (requestType) {
         break;
 
     case "movie-this":
+      // if no movie was entered, default to Mr. Nobody
+      if (searchItem === undefined) {
+        searchItem = "Mr. Nobody";
+      }
+
+      var queryURL = "http://www.omdbapi.com/?apikey=" + keys.obdb_key + "&t=" + searchItem;
+
+      axios.get(queryURL).then(function(response) {
+        if (response.data.Error) {
+          console.log(response.data.Error);
+          return false;
+        }
+        console.log("  ------------  ");
+        console.log("Movie Title: " + response.data.Title);
+        console.log("Year Released: " + moment(response.data.Released, "DD MMM YYYY").format("YYYY"));
+        console.log("Rating: " + response.data.Rated.toUpperCase());
+        console.log("IMDB Rating: " + response.data.imdbRating)
+        response.data.Ratings.forEach(element => {
+          if (element.Source === "Rotten Tomatoes") {
+            console.log("Rotten Tomatoes Rating: " + element.Value);
+          }
+        });
+        console.log("Country Produced: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+        console.log("  ------------  ");
+      });
         break;
     
     case "do-what-it-says":
+
         break;
 
     default:
